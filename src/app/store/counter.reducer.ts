782482883a135e3f0ace9c333f890ab99f3f8fa1
smoonmare <smoonmare@gmail.com>
2023-07-13 13:10:51 -0700

    Implement '@ngrx/store' Actions and Remove 'CounterService'
    In this commit, the counter actions are being implemented and dispatched via '@ngrx/store'. 'CounterControlsComponent' is refactored to use these actions instead of the 'CounterService'. The actions implemented are 'increment' and 'decrement', each triggering the appropriate updates to the state.
    
    Changes include:
    1. '@ngrx/store' is imported in 'CounterControlsComponent' to replace 'CounterService'. 'increment' and 'decrement' methods now dispatch actions instead of using methods from 'CounterService'.
    2. The 'CounterService' file has been removed, as its functionalities are now provided by '@ngrx/store'.
    3. A new file 'counter.actions.ts' is added to define the actions 'increment' and 'decrement'.
    4. In 'counter.reducer.ts', the reducer function is updated to handle 'increment' and 'decrement' actions using the 'createReducer' and 'on' methods.
    
    These changes integrate '@ngrx/store' into the counter controls and eliminate the need for the 'CounterService'.

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,11 +1,14 @@
-// import { createReducer } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
+import { decrement, increment } from "./counter.actions";
 
 const initialState = 0;
 
-// export const counterReducer = createReducer(
-//   initialState
-// );
+export const counterReducer = createReducer(
+  initialState,
+  on(increment, (state, action) => state + action.value),
+  on(decrement, (state) => state - 1) 
+);
 
-export function counterReducer(state = initialState) {
-  return state;
-}
+// export function counterReducer(state = initialState) {
+//   return state;
+// }
